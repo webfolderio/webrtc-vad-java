4f11761c9dfb1f7041501cce4b3ac71a16214f6c
WebFolder OÜ <support@webfolder.io>
2020-11-02 22:17:54 +0300

    Update build.bat

--- java/build.bat
+++ java/build.bat
@@ -1,4 +1,4 @@
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_TOOLCHAIN_FILE="C:\tools\vcpkg\scripts\buildsystems\vcpkg.cmake" .. -GNinja
-ninja
+cmake -DCMAKE_BUILD_TYPE=Release -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_TOOLCHAIN_FILE="C:\tools\vcpkg\scripts\buildsystems\vcpkg.cmake" -G"Visual Studio 15 2017 Win64" ..
+cmake --build . --target vad --config Release

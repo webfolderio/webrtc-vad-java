e70f4ff4199bf1091263c7f407f72193ccbcc824
webfolderio <support@webfolder.io>
2020-11-03 20:40:24 +0300

    Fixed

--- java/src/main/java/io/webfolder/webrtc/vad/WebRtcVad.java
+++ java/src/main/java/io/webfolder/webrtc/vad/WebRtcVad.java
@@ -1,21 +1,9 @@
 package io.webfolder.webrtc.vad;
 
-import static java.lang.System.getProperty;
-
-import static java.util.Locale.ENGLISH;
-
 public class WebRtcVad {
 
-    private static final String  OS_NAME  = getProperty("os.name").toLowerCase(ENGLISH);
-
-    private static final boolean WINDOWS  = OS_NAME.startsWith("windows");
-
-    static {
-        if (WINDOWS) {
-            System.loadLibrary("webrtc-vad-java");
-        } else {
-            System.loadLibrary("webrtc-vad-java");
-        }
+	static {
+        System.loadLibrary("webrtc-vad-java");
     }
 
     public static native void initializeVad(int mode);
